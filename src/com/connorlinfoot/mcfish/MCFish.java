88d93b23af81d5d66630bbb9534085cfdd28e304
Connor Linfoot <connorlinfoot@me.com>
2015-02-04 00:07:17 +0000

    Couple of changes: - Teleports 1 block lower than it did before, this may change again - All squids spawned will currently turn into fish

--- src/com/connorlinfoot/mcfish/MCFish.java
+++ src/com/connorlinfoot/mcfish/MCFish.java
@@ -1,14 +1,13 @@
 package com.connorlinfoot.mcfish;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.Server;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -20,7 +19,7 @@ import java.util.HashMap;
 
 
 public class MCFish extends JavaPlugin implements Listener {
-    HashMap<Squid, ArmorStand> hashMap = new HashMap<Squid, ArmorStand>();
+    public static HashMap<Squid, ArmorStand> hashMap = new HashMap<Squid, ArmorStand>();
 
     public void onEnable() {
         getConfig().options().copyDefaults(true);
@@ -47,14 +46,27 @@ public class MCFish extends JavaPlugin implements Listener {
                     ArmorStand armorStand = hashMap.get(squid);
                     if (squid.isDead() || armorStand.isDead()) {
                         hashMap.remove(squid);
+                        squid.remove();
+                        armorStand.remove();
                         continue;
                     }
-                    armorStand.teleport(squid);
+                    Location location = squid.getLocation();
+                    location.setY(location.getBlockY() - 1);
+                    armorStand.teleport(location);
                 }
             }
         };
 
         bukkitRunnable.runTaskTimer(this, 1L, 1L);
+    }
+
+    @EventHandler
+    public void onSquidSpawn(EntitySpawnEvent event) {
+        Entity entity = event.getEntity();
+        if (entity.getType() == EntityType.SQUID) {
+            spawnFish(entity.getLocation());
+            entity.remove();
+        }
     }
 
     @Override
@@ -65,8 +77,27 @@ public class MCFish extends JavaPlugin implements Listener {
         }
 
         Player player = (Player) sender;
+        if (args.length != 1) {
+            spawnFish(player.getLocation());
+        } else {
+            spawnFish(player.getLocation(), Integer.parseInt(args[0]));
+        }
 
-        Entity entity = player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
+        return false;
+    }
+
+    public void onDisable() {
+        getLogger().info(getDescription().getName() + " has been disabled!");
+    }
+
+    public static void spawnFish(Location location, Integer count) {
+        for (int i = 0; i < count; i++) {
+            spawnFish(location);
+        }
+    }
+
+    public static void spawnFish(Location location) {
+        Entity entity = location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
         ArmorStand armorStand = (ArmorStand) entity;
 
         armorStand.setVisible(false);
@@ -81,17 +112,11 @@ public class MCFish extends JavaPlugin implements Listener {
         armorStand.setRightArmPose(eulerAngle);
         armorStand.setLeftArmPose(eulerAngle);
 
-        Entity entity1 = player.getWorld().spawnEntity(player.getLocation(), EntityType.SQUID);
+        Entity entity1 = location.getWorld().spawnEntity(location, EntityType.SQUID);
         Squid squid = (Squid) entity1;
         squid.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 72000, 72000, true));
 
         hashMap.put(squid, armorStand);
-
-        return false;
-    }
-
-    public void onDisable() {
-        getLogger().info(getDescription().getName() + " has been disabled!");
     }
 
 }
